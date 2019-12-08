0971d7c1b4c34c4738540396927fe736fc1758f8
CiyLei <903736662@qq.com>
2019-12-08 13:43:56 +0800

    add: 首次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div style="padding: 20px">
+        <div v-if="this.devicesGroup.length == 0">
+            暂无设备
+        </div>
+        <el-collapse accordion v-for="item in devicesGroup" v-bind:key="item.deviceCode">
+            <el-collapse-item>
+                <template slot="title">
+                    <div style="display: inline-block; width: 35%">
+                        {{item.deviceName}}{{alias(item)}}
+                    </div>
+                    <div style="display: inline-block; width: 35%">
+                        设备唯一识别码：{{item.deviceCode}}
+                    </div>
+                    <div style="display: inline-block; width: 30%; text-align: right">
+                        <el-link style="margin-right: 20px" type="primary" :underline="false" @click="setAlias(item.deviceCode, item.alias, $event)">设置别名</el-link>
+                        <el-link style="margin-right: 20px" type="primary" :underline="false" @click="shock(item.deviceCode, $event)">震动反馈</el-link>
+                    </div>
+                </template>
+                <div v-for="app in item.appInfoList" v-bind:key="app.applicationName">
+                    <div style="display: inline-block; width: 35%">
+                        <el-link type="primary" v-bind:href="'http://' + app.address + ':' + app.port" :underline="false" target="view_window">{{app.applicationName}}</el-link>
+                    </div>
+                    <div style="display: inline-block; width: 65%">
+                        {{app.address}}:{{app.port}}
+                    </div>
+                </div>
+            </el-collapse-item>
+        </el-collapse>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    import * as URL from './UrlConstant'
+    export default {
+        name: 'app',
+        data() {
+            return {
+                devicesGroup: []
+            }
+        },
+        methods: {
+            getDevice() {
+                this.axios.get(URL.GET_DEVICE).then((resp) => {
+                    if (resp.data.success) {
+                        this.devicesGroup = resp.data.data
+                    } else {
+                        this.$message({
+                            showClose: true,
+                            message: resp.data.message,
+                            type: 'error',
+                        })
+                    }
+                })
+            },
+            setAlias(code, alias, e) {
+                this.$prompt('请输入别名', "设备唯一识别码：" + code, {
+                    inputValue: alias
+                }).then(({ value }) => {
+                    this.axios.get(URL.SET_ALIAS, {
+                        params: {
+                            code: code,
+                            alias: value
+                        }
+                    }).then((resp) => {
+                        if (resp.data.success) {
+                            this.getDevice()
+                        } else {
+                            this.$message({
+                                showClose: true,
+                                message: resp.data.message,
+                                type: 'error',
+                            })
+                        }
+                    })
+                })
+                e.cancelBubble = true;
+            },
+            shock(code, e) {
+                this.axios.get(URL.SHOCK, {
+                    params: {
+                        code: code
+                    }
+                }).then((resp) => {
+                    if (!resp.data.success) {
+                        this.$message({
+                            showClose: true,
+                            message: resp.data.message,
+                            type: 'error',
+                        })
+                    }
+                })
+                e.cancelBubble = true;
+            },
+            alias(item) {
+                if ("" === item.alias) {
+                    return ""
+                }
+                return "（" + item.alias + "）"
+            },
+        },
+        mounted() {
+            this.getDevice()
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/UrlConstant.js
@@ -0,0 +1,4 @@
+const PREFIX = ""
+export const GET_DEVICE = PREFIX + "/device"
+export const SHOCK = PREFIX + "/shock"
+export const SET_ALIAS = PREFIX + "/setAlias"
